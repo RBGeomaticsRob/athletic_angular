47c5943ce9ca17c16217ee6f5bc43e5a83473ef5
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-05-01 08:05:18 +0100

    Setup app and brought in ngresource

---
+++ js/app.js
@@ -0,0 +1,3 @@
+var githubUserSearch = angular.module('GitUserSearch', ['ngResource']);
+// creating an application called 'GitUserSearch'
+// brings in the ngResource module which deals with RESTful API Calls
